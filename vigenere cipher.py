ad4a9132056a4c538624839f780116d4b545700c
rizza <denostarizza43@gmail.com>
2023-04-19 01:37:43 +1200

    process for ciphertext

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -9,4 +9,14 @@ key = input("Keyword: ")
 # Repeat the keyword to match the lenght of the plaintext
 repeated_keyword = key * (len(plain_text) // len(key) + 1)
 keyword = repeated_keyword[:len(plain_text)]
+
+ciphertext = ""
+for i in range(len(plain_text)):
+   plaintext_letter = plain_text[i]
+   keyword_letter = keyword[i]
+   plaintext_index = alphabet.index(plaintext_letter)
+   keyword_index = alphabet.index(keyword_letter)
+   ciphertext_index = (plaintext_index + keyword_index) % 26
+   ciphertext_letter = alphabet[ciphertext_index]
+   ciphertext += ciphertext_letter
 # Print output
