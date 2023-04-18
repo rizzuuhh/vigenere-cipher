0e04d991f45c61326089841c92eb119394980679
rizza <denostarizza43@gmail.com>
2023-04-19 00:29:16 +1200

    keyword and plaintext lenght

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -7,4 +7,6 @@ plain_text = input("Message: ")
 key = input("Keyword: ")
 
 # Repeat the keyword to match the lenght of the plaintext
+repeated_keyword = key * (len(plain_text) // len(key) + 1)
+keyword = repeated_keyword[:len(plain_text)]
 # Print output
