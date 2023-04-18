096ede46886c838d7bdfc02228cfdb33d94dff9e
rizza <denostarizza43@gmail.com>
2023-04-19 00:21:05 +1200

    ask user input

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -1,5 +1,10 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 # Get the plaintext and keyword from the user
 # Message: THISISTHELASTTASKHOORDAY
 # Keyword: KNIGHTS
+plain_text = input("Message: ")
+key = input("Keyword: ")
+
 # Repeat the keyword to match the lenght of the plaintext
 # Print output
